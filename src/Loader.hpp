aae1c0b54593aa887196ee5d575e8f043f5c2833
Oursegamin <axel.lavrador@gmail.com>
2026-03-18 15:17:11 +0100

    fix: change TextureId to Texture

--- src/Loader.hpp
+++ src/Loader.hpp
@@ -27,7 +27,7 @@ namespace loader {
             void _createVelocityCpn(const entt::entity& entity, const nlohmann::json& components);
             void _createNameCpn(const entt::entity& entity, const nlohmann::json& components);
             void _createRadiusCpn(const entt::entity& entity, const nlohmann::json& components);
-            void _createTextureIdCpn(const entt::entity& entity, const nlohmann::json& components);
+            void _createTextureCpn(const entt::entity& entity, const nlohmann::json& components);
 
             std::unordered_map<std::string, std::function<void(const entt::entity&, const nlohmann::json&)>> _mapLoader;
             common::LoaderStatus _status;
