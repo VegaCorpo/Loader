991158a5093d87cba7206952ba57668dd574dc10
Yuuko <146713844+TilioHuart@users.noreply.github.com>
2026-09-25 15:43:16 +0900

    feat: use an interface instead of createscene method (#11)
    * refactor: change texture include path
    
    * feat: use an interface instead of createscene method
    
    * chore: update common version
    
    * chore: tag latest common version
    
    ---------
    
    Co-authored-by: TilioHuart <tilio.huart@epiteh.eu>

--- src/LoaderAPI.cpp
+++ src/LoaderAPI.cpp
@@ -1,17 +1,11 @@
 #include "LoaderAPI.hpp"
-#include <entt/entity/registry.hpp>
-#include <types/types.hpp>
+#include <memory>
 #include "Loader.hpp"
+#include "interfaces/ILoaderEngine.hpp"
 
 extern "C" {
-    common::LoaderStatus createScene(void* registry_ptr, const std::string& filename) noexcept
+    std::unique_ptr<common::ILoaderEngine> get_engine()
     {
-        auto& registry = *static_cast<entt::registry*>(registry_ptr);
-        loader::SimulationLoader loader(registry, filename);
-        if (loader.getLoaderStatus() == common::LoaderStatus::FAILED_TO_OPEN_FILESTREAM)
-            return common::LoaderStatus::FAILED_TO_OPEN_FILESTREAM;
-        loader.registerMapLoader();
-        loader.createEntities();
-        return common::LoaderStatus::SUCCESS;
+        return std::make_unique<loader::SimulationLoader>();
     }
 }
